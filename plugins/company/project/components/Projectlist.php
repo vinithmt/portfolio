2435dd89baa7eab8ec85fa4cfb779b5318076586
vinith m t <vinith.m.t@gmail.com>
2018-06-09 23:12:09 -0500

    timeline projects list in modal window for each company

--- plugins/company/project/components/Projectlist.php
+++ plugins/company/project/components/Projectlist.php
@@ -3,8 +3,9 @@
 use Cms\Classes\ComponentBase;
 use Individuart\Work\Models\Work;  
 use Company\Project\Models\Project;
-
-use Individuart\Work\Models\Category;
+use Individuart\Work\Models\Category; 
+use Octo\Timeline\Models\Entries;
+use Db; 
 use Str;
 
 class Projectlist extends ComponentBase
@@ -22,14 +23,28 @@ class Projectlist extends ComponentBase
     public function onRun()
     {
          $this->projectlist = $this->loadCompanyProjects();
-
+  
     }
 
  
     protected  function loadcompanyProjects(){
-     	   $companyProjects = Project::select('company_id')->distinct()->get();
-     	 //  dump($companyProjects);
-     	 return  $companyProjects ;
+        $companyProjects = array();
+        $company =  Entries::all() ;
+        
+        foreach ($company as $key => $value) {
+             $work =   DB::table('individuart_work_works')
+            ->join('company_project_projects', 'company_project_projects.work_id', '=', 'individuart_work_works.id') 
+            ->select('individuart_work_works.*')
+            ->where('company_project_projects.company_id',$value->id)
+            ->get(); 
+            $company[$key] = array_merge((array)$company[$key], array('works' => $work)) ;
+            
+        }
+
+        $companyProjects['company'] =  $company ;
+    //  print_r($companyProjects);
+      
+     	return  $companyProjects ;
  
     }
 
